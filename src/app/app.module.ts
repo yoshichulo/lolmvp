0db752db1487fab12b09059db0fce1b5ada475b6
Yoshi <joseangelyoshi@gmail.com>
2020-11-05 01:11:34 +0100

    Added missing HttpClientModule. CORS call not fixed yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
 import { SummonerComponent } from './components/summoner/summoner.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,12 +16,13 @@ import {FormsModule} from '@angular/forms';
     SummonerComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/summoner/summoner.component.ts
+++ src/app/components/summoner/summoner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-summoner',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummonerComponent implements OnInit {
 
-  constructor() { }
+  private summonerSearchURL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.fetchData();
+  }
+  fetchData(): void {
+    let headers = new HttpHeaders();
+    headers.append('X-Riot-Token', environment.riotApiKey);
+    headers.append('Access-Control-Allow-Origin', '*');
+    const name = this.route.snapshot.paramMap.get('summonerName');
+    const data = this.httpClient.get(this.summonerSearchURL + name, {headers});
+    data.subscribe((d: any[]) => {
+      console.log(d);
+    });
   }
 
 }
